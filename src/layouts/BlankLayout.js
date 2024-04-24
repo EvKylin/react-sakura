d770957f5273d2d7a916f77774f0776a6ddaaf89
qhanw <whenhan@foxmail.com>
2024-04-24 14:38:49 +0800

    chore: clean repository

--- src/layouts/BlankLayout.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by QiHan Wang on 2017/12/25.
- * E-Mail: whenhan@foxmail.com
- * File Name: BasicLayout
- */
--- src/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-import { reducerMethods as global } from "./global";
-import { reducerMethods as user } from "./user";
-import { reducerMethods as login } from "./login";
-const reducers = {
-  global,
-  user,
-  login
-};
-export default reducers;
